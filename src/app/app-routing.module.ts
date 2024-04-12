d52558f7b164cf01919fe3141d53671a1005c1e8
Your Name <you@example.com>
2024-04-12 20:30:33 +0200

    creation of home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './container/pages/home/home.component';
+import { AboutUsComponent } from './container/pages/about-us/about-us.component';
+import { ServicesComponent } from './container/pages/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'services', component: ServicesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './container/components/nav/nav.component';
 import { FooterComponent } from './container/components/footer/footer.component';
+import { HomeComponent } from './container/pages/home/home.component';
+import { AboutUsComponent } from './container/pages/about-us/about-us.component';
+import { ServicesComponent } from './container/pages/services/services.component';
+import { LoginComponent } from './container/pages/login/login.component';
+import { SignupComponent } from './container/pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
