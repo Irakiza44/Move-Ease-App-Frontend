3e03bc333041bf7a2301c675f0b12c4f82eb3aa7
Your Name <you@example.com>
2024-04-18 16:44:02 +0200

    integration of apis at admin section

--- src/app/container/pages/admin-dashboard/dashboard/dashboard.component.ts
+++ src/app/container/pages/admin-dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../../../services/admin.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  totalUsers: number = 0;
+  totalContacts: number = 0;
+  totalBackups: number = 0;
 
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.adminService.getUsers().subscribe(
+      (usersResponse) => {
+        this.totalUsers = usersResponse.totalUsers;
+      },
+      (error) => {}
+    );
+
+    this.adminService.getContacts().subscribe(
+      (contactsResponse) => {
+        this.totalContacts = contactsResponse.totalUsers;
+      },
+      (error) => {}
+    );
+    this.adminService.getBackupContacts().subscribe(
+      (backupContactsResponse) => {
+        this.totalBackups = backupContactsResponse.totalUsers;
+      },
+      (error) => {}
+    );
+  }
 }
